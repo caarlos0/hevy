d0f5343523d1e8f759b904b6b6bde48f9bf52eaf
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2026-05-04 14:20:27 -0300

    ci: fix bump
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -5,4 +5,4 @@ version="$(svu next --always)"
 npm version "${version#v}" --no-git-tag-version
 git add package.json package-lock.json
 git commit -m "release: $version"
-git tag "$version"
+git tag "$version" -m "$version"
