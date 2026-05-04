9532110601969b8bc955225135aa1944280bd0ae
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2026-05-04 14:20:09 -0300

    ci: fix release
    Signed-off-by: Carlos Alexandro Becker <caarlos0@users.noreply.github.com>

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -e
 
-version="$(svu next)"
+version="$(svu next --always)"
 npm version "${version#v}" --no-git-tag-version
 git add package.json package-lock.json
 git commit -m "release: $version"
